25e867d614ef51d412f59cc4d88da577596f3905
S.M. Mostofa Morshed Sayeem <198875870+MostofaMorshedSayeem@users.noreply.github.com>
2025-07-08 23:50:19 +0600

    updates
    updt

---
+++ Course/CS111/Lab/Week 1/Home/Task3.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int input = scanner.nextInt();
+
+        int array[] = new int[input];
+
+        for (int i = 0; i < input; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int i = input - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+
+        scanner.close();
+
+    }
+}
